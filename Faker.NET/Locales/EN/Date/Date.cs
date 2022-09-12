a0dca0b519995e6ab1e34acdb7fe9e7a012b8575
baileydauterman <dauterman.bailey@gmail.com>
2022-09-12 19:15:43 -0400

    add date time

--- Faker.NET/Locales/EN/Date/Date.cs
+++ Faker.NET/Locales/EN/Date/Date.cs
@@ -4,20 +4,33 @@ namespace Faker.NET.EN.Date
 {
     public static class Date
     {
-        public static string Day
-        {
-            get => day.Get();
-        }
+        public static string Day => day.Get();
 
         public static string DayAbbreviated => day.Get(true);
 
-        public static string DateNum => Randomizer.Next(1,31).ToString();
+        public static string DateNum => Randomizer.Next(1, 31).ToString();
 
         public static string Month => month.Get();
 
         public static string MonthAbbreviated => month.Get(true);
 
-        public static string Year => Randomizer.Next(1995, 2025).ToString();
+        public static string MonthNum => Randomizer.Next(1, 12).ToString();
+
+        public static string Year => Randomizer.Next(YearMin, YearMax).ToString();
+
+        public static string Hour => Randomizer.Next(1, 12).ToString();
+
+        public static string Hour24 => Randomizer.Next(1, 24).ToString();
+
+        public static string Minute => Randomizer.Next(0, 60).ToString();
+
+        public static string Second => Randomizer.Next(0, 60).ToString();
+
+        public static string Formatted(string format)
+        {
+            var dt = new DateTime(int.Parse(Year), int.Parse(MonthNum), int.Parse(DateNum), int.Parse(Hour), int.Parse(Minute), int.Parse(Second));
+            return dt.ToString(format);
+        }
 
         public static string NowFormatted(string format)
         {
@@ -26,5 +39,10 @@ namespace Faker.NET.EN.Date
 
         private static Month month = new();
         private static Day day = new();
+        private static Time time = new();
+
+
+        public static int YearMin { get; set; }
+        public static int YearMax { get;  set; }
     }
 }
--- FakerTestHarness/English/Dates.cs
+++ FakerTestHarness/English/Dates.cs
@@ -19,21 +19,22 @@ namespace FakerTestHarness.English
             Console.WriteLine(Date.NowFormatted("MM/dd/yyyy"));
             Console.WriteLine(Date.NowFormatted("dddd, dd MMMM yyyy"));
             Console.WriteLine(Date.NowFormatted("dddd, dd MMMM yyyy HH:mm:ss"));
-            Console.WriteLine(Date.NowFormatted("MM/dd/yyyy HH:mm"));
-            Console.WriteLine(Date.NowFormatted("MM/dd/yyyy hh:mm tt"));
-            Console.WriteLine(Date.NowFormatted("MM/dd/yyyy H:mm"));
-            Console.WriteLine(Date.NowFormatted("MM/dd/yyyy h:mm tt"));
-            Console.WriteLine(Date.NowFormatted("MM/dd/yyyy HH:mm:ss"));
-            Console.WriteLine(Date.NowFormatted("MMMM dd"));
-            Console.WriteLine(Date.NowFormatted("yyyy’-‘MM’-‘dd’T’HH’:’mm’:’ss.fffffffK"));
-            Console.WriteLine(Date.NowFormatted("ddd, dd MMM yyy HH’:’mm’:’ss ‘GMT’"));
-            Console.WriteLine(Date.NowFormatted("yyyy’-‘MM’-‘dd’T’HH’:’mm’:’ss"));
             Console.WriteLine(Date.NowFormatted("HH:mm"));
             Console.WriteLine(Date.NowFormatted("hh:mm tt"));
             Console.WriteLine(Date.NowFormatted("H:mm"));
             Console.WriteLine(Date.NowFormatted("h:mm tt"));
             Console.WriteLine(Date.NowFormatted("HH:mm:ss"));
             Console.WriteLine(Date.NowFormatted("yyyy MMMM"));
+
+            Console.WriteLine(Date.Formatted("MM/dd/yyyy"));
+            Console.WriteLine(Date.Formatted("dddd, dd MMMM yyyy"));
+            Console.WriteLine(Date.Formatted("dddd, dd MMMM yyyy HH:mm:ss"));
+            Console.WriteLine(Date.Formatted("HH:mm"));
+            Console.WriteLine(Date.Formatted("hh:mm tt"));
+            Console.WriteLine(Date.Formatted("H:mm"));
+            Console.WriteLine(Date.Formatted("h:mm tt"));
+            Console.WriteLine(Date.Formatted("HH:mm:ss"));
+            Console.WriteLine(Date.Formatted("yyyy MMMM"));
         }
     }
 }
