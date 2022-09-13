b616c98d5ebd6f680b323a98ff8773645bd9cfb7
baileydauterman <dauterman.bailey@gmail.com>
2022-09-13 16:20:01 -0400

    have less email domains

--- FakerTestHarness/Program.cs
+++ FakerTestHarness/Program.cs
@@ -1,18 +1,16 @@
 ﻿FakerTestHarness.English.Names.TestNames();
+
 Console.WriteLine(Environment.NewLine);
 FakerTestHarness.English.Lorem.Test();
+
 Console.WriteLine(Environment.NewLine);
 FakerTestHarness.English.Dates.Test();
+
 Console.WriteLine(Environment.NewLine);
 FakerTestHarness.English.Locations.Test();
+
 Console.WriteLine(Environment.NewLine);
 FakerTestHarness.English.Computers.Test();
 
-
 Console.WriteLine(Environment.NewLine);
 FakerTestHarness.English.Users.Test();
-//for (int i = 0; i < 10; i++)
-//{
-//    var text = $"<div class=\"most-recent\"><div class=\"hstack gap-2\"><div class=\"fw-bold\">{Name.FullName}</div><div class=\"text-muted ms-auto fw-light\">{ Location.City }, { Location.State}</div></div><div class=\"hstack gap-2\"><div class=\"fst-italic\">{Name.JobTitle}</div><div class=\"text-muted ms-auto fw-light\">Sept2020-Present</div></div><ul><li>{Lorem.GetText(15)}</li><li>{Lorem.GetText(15)}</li><li>{Lorem.GetText(15)}</li></ul></div>";
-//    Console.WriteLine(text);
-//}
