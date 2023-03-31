dc462cab1ae1d51877b44d8394fc18c913507186
bailey <dauterman.bailey@gmail.com>
2023-03-31 17:36:42 -0400

    Implement ability to create a custom data stucture

---
+++ Faker.NET.Tests/Custom/Custom.cs
@@ -0,0 +1,36 @@
+﻿namespace Faker.NET.Tests.Custom
+{
+    internal class Custom
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            data = new List<string>()
+            {
+                "Pizza",
+                "Cheesecake",
+                "Mac & Cheese",
+
+            };
+
+            secondData = new List<string>()
+            {
+                "Milkshake",
+                "Lemonade",
+                "Cola",
+            };
+        }
+
+        [Test]
+        public void CreateCustom()
+        {
+            var custom = new Faker.NET.Custom.Custom(data, secondData);
+
+            _ = custom.Primary;
+            _ = custom.Secondary;
+        }
+
+        List<string> data;
+        List<string> secondData;
+    }
+}
---
+++ Faker.NET/Custom/Custom.cs
@@ -0,0 +1,27 @@
+﻿using Faker.NET.Common;
+
+namespace Faker.NET.Custom
+{
+    /// <summary>
+    /// Allows users to create their own custom data sets to create data off of.
+    /// </summary>
+    public class Custom : FakerText
+    {
+        public Custom(List<string> primaryData, List<string> secondaryData)
+        {
+            if (primaryData is not null)
+            {
+                Native = primaryData;
+            }
+
+            if (secondaryData is not null)
+            {
+                Translated = secondaryData;
+            }
+        }
+
+        public string Primary => base.Get();
+
+        public string Secondary => base.Get(true);
+    }
+}
