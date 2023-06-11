22e20eaa8cb6031b28f0d220542ed6f32dbbb4d2
baileydauterman <50151523+baileydauterman@users.noreply.github.com>
2023-06-11 12:52:11 -0400

    French First and Last Names

---
+++ src/Faker.NET/Locales/FR/Names/Name.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Faker.NET.Locales.FR.Names;
+
+namespace Faker.NET.FR.Names
+{
+	public class Name
+	{
+		public static string FirstName => _firstName.Get();
+		public static string LastName => _lastName.Get();
+		public static string FullName => $"{FirstName} {LastName}";
+
+		private static FirstName _firstName = new();
+		private static LastName _lastName = new();
+	}
+}
+
---
+++ tests/Faker.NET.Tests/FR/Names.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Faker.NET.FR.Names;
+
+namespace Faker.NET.Tests.FR
+{
+	public class Names
+	{
+        [Test]
+        public static void TestNames()
+        {
+            Assert.That(Name.FirstName, Is.Not.Null);
+            Assert.That(Name.LastName, Is.Not.Null);
+            Assert.That(Name.FullName, Is.Not.Null);
+        }
+    }
+}
+
