c51d572b8c5785fbd3c6f95a90e60b9d298d23bc
baileydauterman <50151523+baileydauterman@users.noreply.github.com>
2024-11-27 09:59:56 -0500

    cleanup

--- src/Faker.NET/Interfaces/IFakerCustom.cs
+++ src/Faker.NET/Interfaces/IFakerCustom.cs
@@ -5,7 +5,7 @@
         string[] Data { get; set; }
 
         /// <summary>
-        /// Use <see cref="Common.Extensions.RandomizerExtensions"/> to get random data from values
+        /// Use <see cref="Faker.NET.Extensions.RandomizerExtensions"/> to get random data from values
         /// </summary>
         /// <returns></returns>
         string GetValue();
