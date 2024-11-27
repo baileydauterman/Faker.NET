226223df1e56f5e374541ad4029bda28d4a6dcb4
baileydauterman <50151523+baileydauterman@users.noreply.github.com>
2024-11-27 09:59:21 -0500

    Cleanup

--- src/Faker.NET/Attributes/FakerAttribute.cs
+++ src/Faker.NET/Attributes/FakerAttribute.cs
@@ -1,6 +1,13 @@
 namespace Faker.NET.Attributes;
 
+/// <summary>
+/// Attribute class to use the public facing <see cref="Faker"/> class to generate fake data off of class attributes
+/// </summary>
 public abstract class FakerAttribute : Attribute
 {
+    /// <summary>
+    /// Method to get a value based off of an attribute
+    /// </summary>
+    /// <returns>The object created by <see cref="Faker"/></returns>
     public abstract object GetPropertyValue();
 }
--- src/Faker.NET/Interfaces/IFakerCustom.cs
+++ src/Faker.NET/Interfaces/IFakerCustom.cs
@@ -5,7 +5,7 @@
         string[] Data { get; set; }
 
         /// <summary>
-        /// Use <see cref="global::Faker.NET.Common.RandomizerExtensions"/> to get random data from values
+        /// Use <see cref="Common.Extensions.RandomizerExtensions"/> to get random data from values
         /// </summary>
         /// <returns></returns>
         string GetValue();
